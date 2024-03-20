4c2f6801e81d9c6a1d0394ff810d60a782d41f0f
Kevin Zhang <K.Zhang@ceridian.com>
2024-03-20 17:11:08 -0400

    setup local

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,9 @@ use sqlx::sqlite::SqliteQueryResult;
  const DB_URL:&str = "sqlite://sqlite.db";
 
 
-// async fn create_schema(db_url: &str) -> Result<SqliteQueryResult, sqlx::Error> {
-//     let pool = SqlitePool::connect(&db_url).await?;
-//     let qry =
-//         "
-//     ";
-//
-//
-//     let result = sqlx::query(&qry)
-//         .execute(&pool).await;
-//     println!("----------");
-//     pool.close().await;
-//     return result
-// }
-
-
 #[tokio::main]
 async fn main() {
-    //let db_url = String::from("sqlite://sqlite.db");
-    // db migration
+
     if !Sqlite::database_exists(&DB_URL).await.unwrap_or(false) {
         println!("Creating database {}", &DB_URL);
         match Sqlite::create_database(&DB_URL).await {
@@ -50,18 +34,6 @@ async fn main() {
     }
 
     println!("migration: {:?}", migration_results);
-
-    // if !Sqlite::database_exists(&DB_URL).await.unwrap_or(false) {
-    //     Sqlite::create_database(&DB_URL).await.unwrap();
-    //     match  create_schema(&DB_URL).await  {
-    //         Ok(_) => println!("Schema created"),
-    //         Err(e) => println!("Error creating schema: {:?}", e)
-    //     }
-    // }
-
-
-
-
     let instance = SqlitePool::connect(&DB_URL).await.unwrap();
     let qry = "INSERT INTO settings (description) VALUES($1)";
     let result = sqlx::query(&qry)
